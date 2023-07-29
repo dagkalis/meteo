c65cd72385fe9026dda25e867334db86e5c83599
dagkalis <geodagkalis@gmail.com>
2023-07-29 22:38:42 +0300

    first google chart

--- app/controllers/api/v1/forecasts_controller.rb
+++ app/controllers/api/v1/forecasts_controller.rb
@@ -15,7 +15,7 @@ class Api::V1::ForecastsController < ApplicationController
 
     # https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41
     # Make the API request
-    uri = URI("#{base_url}?latitude=#{latitude}&longitude=#{longitude}&hourly=temperature_2m,rain,showers,cloudcover")
+    uri = URI("#{base_url}?latitude=#{latitude}&longitude=#{longitude}&hourly=temperature_2m")
     Log.d uri
     response = Net::HTTP.get(uri)
 
@@ -27,29 +27,27 @@ class Api::V1::ForecastsController < ApplicationController
 
     # Log.d raw_data
 
-    data = []
+    data = {}
     # Log.d raw_data[:hourly]
     hourly_data = raw_data[:hourly]
     hourly_data[:time].each_with_index do |time, index|
-      # date = DateTime.strptime(hourly_data[:time][index], '%m/%d/%Y %I:%M %p')
-      # Log.d({
-      #   raw: hourly_data[:time][index], 
-      #   parsed: DateTime.iso8601(hourly_data[:time][index]).to_time
-      # })
       date = DateTime.parse(hourly_data[:time][index])
-
-      data << {
-        time: hourly_data[:time][index],
-        date: date.strftime("%d/%m/%Y"),
+      
+      data[date.strftime("%d/%m/%Y")] ||= []
+      data[date.strftime("%d/%m/%Y")] << {
         hour: date.strftime("%H"),
-        temperature: hourly_data[:temperature_2m][index],
-        rain: hourly_data[:rain][index],
-        showers: hourly_data[:showers][index],
-        cloudcover: hourly_data[:cloudcover][index],
+        temp: hourly_data[:temperature_2m][index],
+        # time: hourly_data[:time][index],
+        # date: date.strftime("%d/%m/%Y"),
+        # rain: hourly_data[:rain][index],
+        # showers: hourly_data[:showers][index],
+        # cloudcover: hourly_data[:cloudcover][index],
 
        }
     end
-    
+
+    # Rails.logger.debug data.to_a.to_json
+    # Rails.logger.debug data
     render json: data
   end
 end
